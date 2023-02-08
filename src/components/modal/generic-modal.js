84bdad94ed268c4d417ca1bfa46df06b54c83fcd
Lucas Gouvea <lucasgouvea93@gmail.com>
2023-02-08 11:20:42 -0300

    feat: create driver modal

--- src/components/modal/generic-modal.js
+++ src/components/modal/generic-modal.js
@@ -1,45 +1,43 @@
-import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
-import MuiModal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Typography from '@mui/material/Typography';
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import MuiModal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Typography from "@mui/material/Typography";
 
 const style = {
-    position: 'absolute',
-    top: '30%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    bgcolor: 'background.paper',
-    boxShadow: 24,
-    p: 4,
-    display: "flex",
-    flexDirection: "column"
+  position: "absolute",
+  top: "45%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  flexDirection: "column"
 };
 
 export function GenericModal({ open, onClose, title, children }) {
-    return (
-        <div>
-            <MuiModal
-                aria-labelledby="transition-modal-title"
-                aria-describedby="transition-modal-description"
-                open={open}
-                onClose={onClose}
-                closeAfterTransition
-                BackdropComponent={Backdrop}
-                BackdropProps={{
-                    timeout: 500,
-                }}
-            >
-                <Fade in={open}>
-                    <Box sx={style}>
-                        <Typography id="title" variant="h6" component="h2" alignSelf={"center"}>
-                            {title}
-                        </Typography>
-                        {children}
-                    </Box>
-                </Fade>
-            </MuiModal>
-        </div >
-    );
-
+  return (
+    <div>
+      <MuiModal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={onClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500
+        }}>
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="title" variant="h6" component="h2" alignSelf={"center"}>
+              {title}
+            </Typography>
+            {children}
+          </Box>
+        </Fade>
+      </MuiModal>
+    </div>
+  );
 }
--- src/pages/drivers/create-driver-modal.js
+++ src/pages/drivers/create-driver-modal.js
@@ -1,53 +1,107 @@
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import Checkbox from '@mui/material/Checkbox';
-import Button from '@mui/material/Button';
+import { Button, Checkbox, TextField, Typography, Box, FormControlLabel } from "@mui/material";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
-export default function CreateDriverModal({ onUpdate, onCancel, data }) {
-    return (
-        <>
-            <Box style={{ marginTop: 50, width: 400 }}>
-                <TextField
-                    id="name"
-                    label="Nome"
-                    variant="outlined"
-                    error={false}
-                    helperText={false} //"Incorrect entry."
-                    fullWidth
-                    value={data?.name}
-                />
-            </Box>
+export default function CreateDriverModal({ onCancel }) {
+  const { handleSubmit, handleChange, handleBlur, values, errors, touched } = useFormik({
+    initialValues: {
+      name: "",
+      age: "",
+      licenseA: false,
+      licenseB: false,
+      licenseC: false,
+      licenseD: false
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .max(30, "O nome do motorista deve ter no máximo 30 letras")
+        .required("Este campo é obrigatório"),
+      age: Yup.number()
+        .min(18, "A idade deve ser maior que 18 anos")
+        .max(100, "A idade deve ser menor que 100 anos")
+        .required("Este campo é obrigatório"),
+      licenseA: Yup.boolean(),
+      licenseB: Yup.boolean(),
+      licenseC: Yup.boolean(),
+      licenseD: Yup.boolean()
+    })
+  });
 
-            <Box style={{ marginTop: 20, width: 400 }}>
-                <Typography id="title" variant="h8">
-                    Cartas:
-                </Typography>
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <Checkbox checked={data?.licenses.find((license) => license === "A")} />A
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <Checkbox checked={data?.licenses.find((license) => license === "B")} />B
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <Checkbox checked={data?.licenses.find((license) => license === "C")} />C
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <Checkbox checked={data?.licenses.find((license) => license === "D")} />D
-            </Box>
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <Box style={{ marginTop: 50, width: 400 }}>
+          <TextField
+            id="name"
+            label="Nome"
+            variant="outlined"
+            error={touched.name && errors.name}
+            helperText={errors.name || "Insira o nome do motorista"}
+            fullWidth
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.name}
+          />
+        </Box>
 
-            <Box style={{ marginTop: 20, width: 100 }}>
-                <TextField
-                    id="age"
-                    label="Idade"
-                    variant="outlined"
-                    error={false}
-                    helperText={false} //"Incorrect entry."
-                    value={data?.age}
-                />
-            </Box>
+        <Box style={{ marginTop: 20, width: 400 }}>
+          <Typography id="title" variant="h8">
+            Cartas:
+          </Typography>
+          &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+          <FormControlLabel
+            control={<Checkbox checked={values.licenseA} />}
+            label="A"
+            name="licenseA"
+            onChange={handleChange}
+          />
+          <FormControlLabel
+            control={<Checkbox checked={values.licenseB} />}
+            label="B"
+            name="licenseB"
+            onChange={handleChange}
+          />
+          <FormControlLabel
+            control={<Checkbox checked={values.licenseC} />}
+            label="C"
+            name="licenseC"
+            onChange={handleChange}
+          />
+          <FormControlLabel
+            control={<Checkbox checked={values.licenseD} />}
+            label="D"
+            name="licenseD"
+            onChange={handleChange}
+          />
+        </Box>
 
-            <Box style={{ marginTop: 50, display: "flex", width: "100%", justifyContent: "space-around" }}>
-                <Button variant="contained" onClick={onUpdate}>Atualizar</Button>
-                <Button variant="outlined" onClick={onCancel}>Cancelar</Button>
-            </Box>
-        </>
-    )
+        <Box style={{ marginTop: 20, width: 100 }}>
+          <TextField
+            id="age"
+            label="Idade"
+            variant="outlined"
+            error={touched.age && errors.age}
+            helperText={errors.age || "Insira a idade do motorista"}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.age}
+            type="number"
+          />
+        </Box>
+
+        <Box
+          style={{ marginTop: 50, display: "flex", width: "100%", justifyContent: "space-around" }}>
+          <Button variant="contained" type="submit">
+            Atualizar
+          </Button>
+          <Button variant="outlined" onClick={onCancel}>
+            Cancelar
+          </Button>
+        </Box>
+      </form>
+    </>
+  );
 }
